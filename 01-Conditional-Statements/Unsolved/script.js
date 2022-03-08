23e9a52333e2e9e64079f854795605ed7159164e
Spencer Jagernauth <rita.jagernauth01@gmail.com>
2022-03-07 21:01:01 -0500

    added js

--- 01-Conditional-Statements/Unsolved/script.js
+++ 01-Conditional-Statements/Unsolved/script.js
@@ -1,6 +1,21 @@
 // TODO: Change the values and operators below to test your algorithm meets all conditions
 var x = 50;
-var expression1 = x < 25;
-var expression2 = x > 50;
+var expression1 = x > 25; 
+var expression2 = x > 50; 
 
 // TODO: Write Your JavaScript Code Here
+// if both expressions are true, log ""True ✅ True ✅"
+if (expression1 === true && expression2 ===true) {
+    console.log("True ✅ True ✅");
+}
+
+else if(expression 1 === true &&& expression2 false){
+    console.log("True  ✅ False ❌");
+
+}
+else if (expression2 === true && expression 1 === false){
+    console.log ("False ❌ True  ✅ ");
+    else {
+        console.log ("False ❌ False ❌")
+    }
+}
